0a3eac2af4b8e2715bd074121cf86d51d504757f
Shamis Shukoor <shamis657@gmail.com>
2016-09-27 14:21:54 +0200

    src/esaml_binding.erl

--- src/esaml_binding.erl
+++ src/esaml_binding.erl
@@ -73,23 +73,13 @@ encode_http_post(IdpTarget, SignedXml, RelayState) ->
     generate_post_html(Type, IdpTarget, base64:encode(Req), RelayState).
 
 generate_post_html(Type, Dest, Req, RelayState) ->
-    iolist_to_binary([<<"<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">
-<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">
-<head>
-<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" />
-<title>POST data</title>
-</head>
-<body onload=\"document.forms[0].submit()\">
-<noscript>
-<p><strong>Note:</strong> Since your browser does not support JavaScript, you must press the button below once to proceed.</p>
-</noscript>
-<form method=\"post\" action=\"">>,Dest,<<"\">
-<input type=\"hidden\" name=\"">>,Type,<<"\" value=\"">>,Req,<<"\" />
-<input type=\"hidden\" name=\"RelayState\" value=\"">>,RelayState,<<"\" />
-<noscript><input type=\"submit\" value=\"Submit\" /></noscript>
-</form>
-</body>
-</html>">>]).
+    iolist_to_binary([<<"
+        <form id=\"samlForm\" method=\"post\" action=\"">>,Dest,<<"\">
+        <input type=\"hidden\" name=\"">>,Type,<<"\" value=\"">>,Req,<<"\" />
+        <input type=\"hidden\" name=\"RelayState\" value=\"">>,RelayState,<<"\" />
+        <noscript><input type=\"submit\" value=\"Submit\" /></noscript>
+        </form>
+    ">>]).
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
